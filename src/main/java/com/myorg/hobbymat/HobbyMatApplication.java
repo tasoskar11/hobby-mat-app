8fc63941566bb2d17e13b4adffc116955c8576c7
Anastasios Karagkounis <tasoskar11@gmail.com>
2023-03-24 17:31:16 +0100

    Add swagger2 support

--- src/main/java/com/myorg/hobbymat/HobbyMatApplication.java
+++ src/main/java/com/myorg/hobbymat/HobbyMatApplication.java
@@ -2,8 +2,13 @@ package com.myorg.hobbymat;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
 
 
 @EnableMongoRepositories
@@ -13,5 +18,14 @@ public class HobbyMatApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(HobbyMatApplication.class, args);
 	}
+
+	@Bean
+    public Docket api() { 
+        return new Docket(DocumentationType.SWAGGER_2)  
+          .select()                                  
+          .apis(RequestHandlerSelectors.any())              
+          .paths(PathSelectors.any())                          
+          .build();                                           
+    }
 
 }
